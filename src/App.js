97ef852419d5277b392e0debe2df25092b598137
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-05-03 17:57:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import BigComponent from "./components/BigComponent";
+import Loading from "./components/Loading";
+import React from "react";
+import { Route,Routes } from "react-router-dom";
+const LazyComponent=React.lazy(()=> import("./components/BigComponent"));
+const App=()=>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Routes>
+        <Route path="/" element={
+          <React.Suspense fallback={<Loading/>}>
+            <LazyComponent/> 
+          </React.Suspense>
+        } />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+
+
+const Loading=()=>{
+    return(
+        <h1>wait until loading.</h1>
+    )
+}
+
+export default Loading;
